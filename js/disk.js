b3f03502aecfa8a3753ffb4abdbf0759a4c339be
wesley <wez@darknet.co.za>
2016-05-26 05:37:56 +0000

    Add local storage disk support.

---
+++ js/disk.js
@@ -0,0 +1,38 @@
+;(function(){
+
+  var disk = window.disk = { };
+  
+  disk.saves = { };
+
+  disk.save = function(key, value) {
+    if (disk.allow) {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    };
+  };
+  
+  disk.load = function(key) {
+    if (disk.allow) {
+      var data = window.localStorage.getItem(key);
+      if (data) {
+        return JSON.parse(data);
+      }
+    };
+  }
+
+  disk.init = function() {
+    try {
+      var storage = window.localStorage,
+        x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      disk.allow = true;
+    }
+    catch(e) {
+      return false;
+    }
+  };
+  
+  disk.init();
+
+})();
+
