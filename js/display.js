4959fc4c58d49298dca335fc0f8243d19b2ace6f
wesley <wez@darknet.co.za>
2016-05-27 12:16:23 +0000

    Show icon

--- js/display.js
+++ js/display.js
@@ -30,6 +30,7 @@
         
         document.getElementById('currentOverview').innerHTML = overview;
         document.getElementById('currentTemp').innerHTML = temp;
+        document.getElementById('weatherIcon').src = 'http://openweathermap.org/img/w/'+data.weather[0].icon+'.png';
 
   }
   
