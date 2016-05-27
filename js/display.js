679a11823277d41317a9283f553a178a8d88f526
wesley <wez@darknet.co.za>
2016-05-27 12:11:26 +0000

    Add a display for weather info

---
+++ js/display.js
@@ -0,0 +1,40 @@
+
+/**
+ * The display updates the UI with the zen weather dtaa.
+ */
+ 
+;(function(){
+  
+  function UpdateUI (data) {
+    
+            
+        // zen.conditions.name
+        // "Durban"
+        // zen.conditions.main.temp
+        // 294.495
+        // zen.conditions.weather[0].description
+        // "clear sky"
+        // zen.conditions.weather[0].icon
+        // "01d"
+        // zen.conditions.weather[0].id
+        // 800
+        // zen.conditions.weather[0].main
+        // "Clear"
+        // zen.conditions.wind.deg
+        // 4.50061
+        // zen.conditions.wind.speed
+        // 4.12
+        
+        var temp = Math.round(data.main.temp-273.15) + ' degrees';
+        var overview = data.name + ', ' + data.weather[0].description;
+        
+        document.getElementById('currentOverview').innerHTML = overview;
+        document.getElementById('currentTemp').innerHTML = temp;
+
+  }
+  
+  // Register our update method with the zen weather method.
+  // We get called every time the weather data changes.
+  zen.callback = UpdateUI;
+  
+})();
