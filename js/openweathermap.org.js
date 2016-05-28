75a63e412b3e9fc84187623358bb7f6e2d44deb8
wesley <wez@darknet.co.za>
2016-05-28 17:08:58 +0000

    Wrap json call in try catch

--- js/openweathermap.org.js
+++ js/openweathermap.org.js
@@ -38,18 +38,24 @@
      * Retrieve JSON data.
      */
     zen.getJSON = function(url, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("get", url, true);
-        xhr.responseType = "json";
-        xhr.onload = function() {
-          var status = xhr.status;
-          if (status == 200) {
-            callback(null, xhr.response);
-          } else {
-            callback(status);
-          }
-        };
-        xhr.send();
+        try {
+            var xhr = new XMLHttpRequest();
+            xhr.open("get", url, true);
+            xhr.responseType = "json";
+            xhr.onload = function() {
+              var status = xhr.status;
+              if (status == 200) {
+                callback(null, xhr.response);
+              } else {
+                callback(status);
+              }
+            };
+            xhr.send();
+        }
+        catch (err) {
+            if (err.result == 2153644038 && err.message == '') err.message = 'Unknown error. Possibly caused by a cross-domain http request, or the target domain is of a varying secure protocol (http/https).';
+            callback(err, null);
+        }
     };
     
     /**
@@ -84,7 +90,7 @@
         // Call the API for weather conditions
         zen.getJSON(APIURL, function(err, data) {
           if (err != null) {
-            alert("Something went wrong: " + err);
+            console.log("Something went wrong: " + err);
           } else {
             
             // Capture the city id, name and coordinates.
