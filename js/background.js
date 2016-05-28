fa1d0b47deb9eb095c24c2b05b71f81f9b7fd517
wesley <wez@darknet.co.za>
2016-05-28 09:24:52 +0000

    Make background still and video configurable at runtime.

--- js/background.js
+++ js/background.js
@@ -1,13 +1,30 @@
 ;(function(){
     
+    var bg = window.zen.background = { };
+    
     // Set the amount of motion blur 0..0.9
-    var motionBlurAmount = 0;
+    bg.motionBlurAmount = 0;
     
     // Video playback rate. 1: normal speed. 0.25 quarter speed.
-    var playbackRate = 0.5;
+    bg.playbackRate = 1;
     
     // Realtime render speed
-    var fastFPS = false;
+    bg.fastFPS = false;
+    
+    // playback quality determines the video played, and the rendering frequency.
+    bg.quality = 0;
+    
+    // the theme determines the video and background images displayed.
+    bg.theme = 'lighthouse';
+    
+    // create the still background image. It stays hidden, and is drawn
+    // to the canvas once loaded.
+    var still = document.createElement('img');
+    still.id = 'still';
+    still.style.display = 'none';
+    document.body.appendChild(still);
+    // draw the still once it has loaded
+    still.addEventListener('load', draw);
     
     // create the background canvas
     var canvas = document.createElement('canvas');
@@ -20,8 +37,8 @@
     // create the video container
     var video = document.createElement('video');
     video.id = "backgroundvideo";
-    video.src = "videos/lighthouse480.webm";
-    video.playbackRate = playbackRate;
+    //video.src = "videos/lighthouse480.webm";
+    video.playbackRate = bg.playbackRate;
     video.loop = true;
     video.addEventListener('play', function(){
         draw();
@@ -30,30 +47,69 @@
     document.body.appendChild(video);
     
     function draw() {
-        if(video.paused || video.ended) return false;
-        context.globalAlpha = 1 - motionBlurAmount;
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        if (fastFPS) {
-            requestAnimationFrame(draw);
+        
+        if (bg.quality == 0) {
+            // Draw the still image to the canvsa
+            context.globalAlpha = 1;
+            context.drawImage(still, 0, 0, canvas.width, canvas.height);
+            //setTimeout(draw, 600);
         }
         else {
-            setTimeout(draw, 60);
+            
+            if(video.paused || video.ended) return false;
+            context.globalAlpha = 1 - bg.motionBlurAmount;
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            if (bg.fastFPS) {
+                requestAnimationFrame(draw);
+            }
+            else {
+                setTimeout(draw, 60);
+            }
+        }
+        
+    }
+    
+    /**
+     * Set the theme and playback quality.
+     * Values include 0, 480, 720, 1080.
+     * 0 displays a still background.
+     */
+    bg.setThemeAndQuality = function(theme, quality) {
+        bg.theme = theme;
+        bg.quality = quality;
+        ApplyThemeAndQuality();
+    }
+    
+    /**
+     * Apply the theme and playback quality settings.
+     */
+    function ApplyThemeAndQuality() {
+        video.pause();
+        if (bg.quality == 0) {
+            still.src = 'backgrounds/beach-sd.jpg';
+            video.src = '';
+            // the "still" element onload will draw to the canvas once loaded.
+        }
+        else {
+            video.src = 'videos/'+bg.theme+bg.quality.toString()+'.webm';
+            initialiseVideo();
         }
     }
 
     function initialiseVideo() {
-        console.log('Loading Video...');
         if (video.readyState >= 3) {
             video.play();
         }
         else {
+            // Retry to play the video
             window.setTimeout(initialiseVideo, 3000);
         }
     }
 
     // start video after a short wait
     document.addEventListener("DOMContentLoaded", function(event) {
-        window.setTimeout(initialiseVideo, 3000);
+        //window.setTimeout(initialiseVideo, 3000);
+        ApplyThemeAndQuality();
     });
     
     
